86d9cf3a211e29b4a906accd6886fc00777eaca0
nando-cezar <luisfernando_cezar@hotmail.com>
2021-01-04 19:38:35 -0300

    data-structure-java-algorithms | queues;

--- Array/src/queues/QueuePriority.java
+++ Array/src/queues/QueuePriority.java
@@ -2,4 +2,17 @@ package queues;
 
 public class QueuePriority<T> extends Queues<T>{
     
+    public void addInQueues(T element){
+        
+        Comparable<T> key = (Comparable<T>) element;
+
+        int i;
+
+        for(i=0; i<this.size;i++){
+            if(key.compareTo(this.elements[i]) < 0){
+                break;
+            }
+        }
+        this.add(i, element);
+    }
 }
